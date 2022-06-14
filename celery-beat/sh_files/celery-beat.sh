21343d63e6b38b4d60ef6c871731fee3ac36c465
mohsenamoon <dramatic225@gmail.com>
2022-06-14 11:56:10 +0430

    adding write permission to /tmp

--- celery-beat/sh_files/celery-beat.sh
+++ celery-beat/sh_files/celery-beat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
 celery -A sportShop beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler --pidfile /tmp/celerybeat.pid
-#chmod +x /sportShop/celerybeat-schedule
+chmod +w /tmp
