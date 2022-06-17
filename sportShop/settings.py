3e9eb073f77b0f8cb9f513109a83204d9266a3c3
mohsenamoon <dramatic225@gmail.com>
2022-06-17 20:36:28 +0430

    changed celery settings

--- sportShop/settings.py
+++ sportShop/settings.py
@@ -140,7 +140,7 @@ SIMPLE_JWT = {
 }
 
 
-CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq3:5672'
+CELERY_BROKER_URL = 'amqp://rabbitmq:5672'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
