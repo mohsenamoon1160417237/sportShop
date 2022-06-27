8731f366a3d15740971cfb292fda99392c96310d
mohsenamoon <dramatic225@gmail.com>
2022-06-27 21:08:37 +0430

    edited celery broker url

--- sportShop/settings.py
+++ sportShop/settings.py
@@ -140,7 +140,7 @@ SIMPLE_JWT = {
 }
 
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
